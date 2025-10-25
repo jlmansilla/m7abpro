dfcd51a7435305a5f1bb74a8f9a93c9e9371b564
jlmansilla <juanmancillaster@gmail.com>
2025-10-25 19:52:08 -0300

    cursos iniciales

---
+++ src/data/iniciales.js
@@ -0,0 +1,66 @@
+// Asumiendo que ya has inicializado Firebase y Firestore
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+
+const db = getFirestore();
+const cursosCollectionRef = collection(db, 'Cursos');
+
+const cursosParaAgregar = [
+  {
+    "codigo": "0001",
+    "nombre": "HTML",
+    "estado": true,
+    "precio": "30000",
+    "duracion": "1 mes",
+    "descripcion": "curso html",
+    "cupos": 10,
+    "inscritos": 0,
+    "img": "https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png"
+  },
+  {
+    "codigo": "0002",
+    "nombre": "CSS",
+    "estado": false,
+    "precio": "20000",
+    "duracion": "1 mes",
+    "descripcion": "curso css",
+    "cupos": 20,
+    "inscritos": 0,
+    "img": "https://lineadecodigo.com/wpcontent/uploads/2014/04/css.png"
+  },
+  {
+    "codigo": "0003",
+    "nombre": "SASS",
+    "estado": true,
+    "precio": "40000",
+    "duracion": "2 mes",
+    "descripcion": "curso sass",
+    "cupos": 30,
+    "inscritos": 0,
+    "img": "https://miro.medium.com/max/512/1*9U1toerFxB8aiFRreLxEUQ.png"
+  },
+  {
+    "codigo": "0004",
+    "nombre": "VUE",
+    "estado": false,
+    "precio": "50000",
+    "duracion": "3 mes",
+    "descripcion": "curso vue",
+    "cupos": 15,
+    "inscritos": 0,
+    "img": "https://thumbs.gfycat.com/PinkPiercingBullsize_restricted.gif"
+  }
+];
+
+async function agregarCursos() {
+  for (const cursoData of cursosParaAgregar) {
+    try {
+      const docRef = await addDoc(cursosCollectionRef, cursoData);
+      console.log("Curso agregado con ID: ", docRef.id, " Datos:", cursoData.nombre);
+    } catch (e) {
+      console.error("Error al agregar el curso", cursoData.nombre, ": ", e);
+    }
+  }
+}
+
+// Llama a esta función para agregar los cursos a Firestore
+agregarCursos();
