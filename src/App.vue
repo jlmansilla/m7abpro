f2d055067b190dd05a4b3bdacc9a8a69c11d5a1f
jlmansilla <juanmancillaster@gmail.com>
2025-10-25 22:35:56 -0300

    refresca deploy

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script setup>
+import { useAuth } from '@/composables/useAuth'
+import { watchEffect } from 'vue'
+import { agregarCursos } from '@/data/iniciales'
 
+const { user } = useAuth()
+
+watchEffect(async () => {
+  if (user.value) {
+    await agregarCursos()
+  }
+})
 </script>
 
 <template>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,6 @@ import './assets/tailwind.css'
 import './style.css'
 import App from './App.vue'
 import router from './router';
-import { agregarCursos } from './data/iniciales';
-
-agregarCursos();
 
 const pinia = createPinia()
 createApp(App).use(pinia).use(router).mount('#app')
