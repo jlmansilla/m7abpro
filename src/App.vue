a3f6e9f363713bee00ed3b3d275173bd2d87e01a
jlmansilla <juanmancillaster@gmail.com>
2025-10-26 22:59:54 -0300

    prueba de workflow

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script setup>
-import { useAuthStore } from '@/stores/auth'
-import { watchEffect } from 'vue'
-import { agregarCursos } from '@/data/iniciales'
+// import { useAuthStore } from '@/stores/auth'
+// import { watchEffect } from 'vue'
+// import { agregarCursos } from '@/data/iniciales'
 
-const authStore = useAuthStore()
+// const authStore = useAuthStore()
 
-watchEffect(async () => {
-  if (authStore.user) {
-    await agregarCursos()
-  }
-})
+// Deshabilitado: agregarCursos() solo se debe ejecutar en desarrollo local
+// watchEffect(async () => {
+//   if (authStore.user) {
+//     await agregarCursos()
+//   }
+// })
 </script>
 
 <template>
